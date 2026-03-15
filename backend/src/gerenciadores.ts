5702396368a2cb28e888dcb9e85acb29b7a58986
GfelixG <g.felix2303@gmail.com>
2026-03-15 09:32:56 -0300

    Criando relacionatentos de tabelas

---
+++ backend/src/gerenciadores.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export class ProfissionalGerenciador {
+  async inserir(nome: string, especialidade: string) {
+    return await prisma.profissional.create({
+      data: { nome, especialidade }
+    });
+  }
+
+  async listarTodos() {
+    return await prisma.profissional.findMany();
+  }
+}
+
+export class ServicoGerenciador {
+  async inserir(descricao: string, preco: number) {
+    return await prisma.servico.create({
+      data: { descricao, preco }
+    });
+  }
+
+  async listarTodos() {
+    return await prisma.servico.findMany();
+  }
+}
+
+export class AgendamentoGerenciador {
+  async agendar(clienteId: number, profissionalId: number, servicoId: number, data: string) {
+    return await prisma.agendamento.create({
+      data: {
+        clienteId,
+        profissionalId,
+        servicoId,
+        dataHora: new Date(data)
+      },
+      include: { // Traz os dados relacionados no retorno
+        cliente: true,
+        profissional: true,
+        servico: true
+      }
+    });
+  }
+
+  async listarRelatorio() {
+    return await prisma.agendamento.findMany({
+      include: {
+        cliente: { select: { nome: true } },
+        profissional: { select: { nome: true } },
+        servico: { select: { descricao: true, preco: true } }
+      }
+    });
+  }
+}
